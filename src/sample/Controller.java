5a0a20188829244b746c67db8866c0ca194cab8e
Anikesh Patel <anikeshpatel4@gmail.com>
2018-01-28 11:40:13 +0530

    init pro

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private WebView window;
+    private WebEngine windowGenerator;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
